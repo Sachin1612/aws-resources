5ec8e35fe2fe72ca59ed344f33ead51cb7406560
Sachin Chouhan <sachin.chouhan@coxautoinc.com>
2024-02-08 15:37:11 +0530

    AWS dynamodb, s3 bucket, serverless lambda

---
+++ DynamoStudentManager/Models/S3ObjectDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamoStudentManager.Models
+{
+    public class S3ObjectDto
+    {
+        public string? Name { get; set; }
+        public string? PresignedUrl { get; set; }
+    }
+}
---
+++ DynamoStudentManager/Program.cs
@@ -0,0 +1,43 @@
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.S3;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DynamoDB
+var awsOptions = builder.Configuration.GetAWSOptions();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+
+// S3 bucket
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
+
+// Serverless API deploy in Lambda
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
